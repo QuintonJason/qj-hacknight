071aab3ceddc495d57d6e17e03fc93b7188edba6
QuintonJason <quinton.jason@gmail.com>
2019-03-15 15:22:19 -0500

    transformed response and passed model date to component

--- app/routes/weather.js
+++ app/routes/weather.js
@@ -4,8 +4,9 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   ajax: service(),
 
-  model() {
-    return this.ajax.request('http://api.aerisapi.com/observations/70816?client_id=AMPdX2M3lA8yrTcPuW78c&client_secret=bvfG6TyVlv7odrhIcgWMbiVdC0UPU9orVEZiGJLW', {
+  async model() {
+    let response = await this.ajax.request('http://api.aerisapi.com/observations/70816?client_id=AMPdX2M3lA8yrTcPuW78c&client_secret=bvfG6TyVlv7odrhIcgWMbiVdC0UPU9orVEZiGJLW', {
     });
+    return response.response;
   }
 });
