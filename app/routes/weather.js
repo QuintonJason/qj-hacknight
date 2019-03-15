e26b89bdf1941fe9f88c5be822385699c0544f94
QuintonJason <quinton.jason@gmail.com>
2019-03-15 14:25:17 -0500

    updated external fetch

--- app/routes/weather.js
+++ app/routes/weather.js
@@ -1,4 +1,11 @@
+import { inject as service } from "@ember/service";
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  ajax: service(),
+
+  model() {
+    return this.ajax.request('http://api.aerisapi.com/observations/70816?client_id=AMPdX2M3lA8yrTcPuW78c&client_secret=bvfG6TyVlv7odrhIcgWMbiVdC0UPU9orVEZiGJLW', {
+    });
+  }
 });
